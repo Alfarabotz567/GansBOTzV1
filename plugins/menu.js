d92b9a634733f66dc6abad27b91851a6c7e98a06
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-09-10 14:13:46 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,63 +1,12 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
-let who
-let user = global.db.data.users[who]
+let moment = require('moment-timezone')
 let fs = require('fs')
-let path = require('path')
-let levelling = require('../lib/levelling')
-let tags = {
-  'main': '*MENU UTAMA*',
-  'advanced': '*ADVANCED*',
-  'rpg': '*MENU RPG*',
-  'anime': '*MENU ANIME*',
-  'sticker': '*MENU CONVERT*',
-  'downloader': '*MENU DOWNLOADER*',
-  'xp': '*MENU EXP*',
-  'fun': '*MENU FUN*',
-  'game': '*MENU GAME*',
-  'github': '*MENU GITHUB*',
-  'group': '*MENU GROUP*',
-  'image': '*MENU IMAGE*',
-  'info': '*MENU INFO*',
-  'absen': '*MENU ABSEN*',
-  'internet': '*INTERNET*',
-  'islam' : '*MENU ISLAMI*',
-  'kerang': '*MENU KERANG*',
-  'maker': '*MENU MAKER*',
-  'owner': '*MENU OWNER*',
-  'quotes' : '*MENU QUOTES*',
-  'stalk': '*MENU STALK*',
-  'tools': '*MENU TOOLS*',
-}
-const wm = `${global.namabot}`
-const instagram = `${instalu}`
-const defaultMenu = {
-  before: ``.trimStart(),
-  header: '⃝',
-  body: '',
-  footer: '',
-  after: `
-*%npmname@^%version*
-${'```%npmdesc```'}
-`,
-}
-let handler = async (m, { conn, usedPrefix: _p }) => {
-  try {
-    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender 
-    let user = global.db.data.users[who]
-    let { exp, limit, level, money, role } = global.db.data.users[m.sender]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let name = conn.getName(m.sender)
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+let name = conn.getName(m.sender)
     let d = new Date(new Date + 3600000)
     let locale = 'id'
-    // d.getTimeZoneOffset()
-    // Offset -420 is 18.00
-    // Offset    0 is  0.00
-    // Offset  420 is  7.00
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let wib = moment.tz('Asia/Jakarta').format('HH:mm:ss')
     let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
     let date = d.toLocaleDateString(locale, {
       day: 'numeric',
       month: 'long',
@@ -84,143 +33,54 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     let muptime = clockString(_muptime)
     let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
-      return {
-        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
-        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-        prefix: 'customPrefix' in plugin,
-        limit: plugin.limit,
-        premium: plugin.premium,
-        enabled: !plugin.disabled,
-      }
-    })
-    for (let plugin of help)
-      if (plugin && 'tags' in plugin)
-        for (let tag of plugin.tags)
-          if (!(tag in tags) && tag) tags[tag] = tag
-    conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || defaultMenu.before
-    let header = conn.menu.header || defaultMenu.header
-    let body = conn.menu.body || defaultMenu.body
-    let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Powered by https://wa.me/6289509960601`) + defaultMenu.after
-    let _text = [
-      before,
-      ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
-          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%islimit/g, menu.limit ? '🅛' : '')
-                .replace(/%isPremium/g, menu.premium ? '🅟' : '')
-                .trim()
-            }).join('\n')
-          }),
-          footer
-        ].join('\n')
-      }),
-      after
-    ].join('\n')
-    text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-    let replace = {
-      '%': '%',
-      p: _p, uptime, muptime,
-      me: conn.user.name,
-      npmname: package.name,
-      npmdesc: package.description,
-      version: package.version,
-      exp: exp - min,
-      maxexp: xp,
-      totalexp: exp,
-      xp4levelup: max - exp,
-      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
-      level, limit, money, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
-      readmore: readMore
-    }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    const anjg = `
+    let waktuwib = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+let bcbg = `${pickRandom(['https://telegra.ph/file/bca700eefeeed8f2cb054.jpg', 'https://telegra.ph/file/ec9831cc3b7001690d6dd.jpg','https://telegra.ph/file/177f7054ebddc6d1f8375.jpg','https://telegra.ph/file/90d4254ae53b4d268b2b9.jpg','https://telegra.ph/file/a6e4013afa98e283ee6a7.jpg','https://telegra.ph/file/2cf9cf86466d9fad58e52.jpg','https://telegra.ph/file/f62c45fb2e087187f065e.jpg','https://telegra.ph/file/fec157267ed3cf69021e1.jpg','https://telegra.ph/file/419672df2fb86a057cb26.jpg'])}`
 
-┌─「 *${wm}* 」
-├ Hai, ${name}
-├ Tersisa *${limit} Limit*
-├ Role *${role}*
-├ Level *${level}
-├ ${exp} XP secara Total
-│ 
-├ Tanggal: *${week} ${weton}, ${date}*
-├ Tanggal Islam: *${dateIslamic}*
-├ Waktu: *${time}*
-│
-├ Uptime: *${uptime}*
-├ Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
-└────
-`
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: anjg.trim(),
-           locationMessage: { 
-           jpegThumbnail: fs.readFileSync('./src/welcome.jpg') },
-           hydratedFooterText: `🅛=limit 🅟=premium`,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '📍Instagram',
-               url: instagram
-             }
+       await conn.delay(1500)
+const anu = `${ucapan()} ${name}`
+const footer = `
+𝙷𝚊𝚕𝚘 ${name} 𝚂𝚊𝚢𝚊 Alfarabotz Multi Device,𝚂𝚊𝚢𝚊 𝙳𝚒 𝙱𝚞𝚊𝚝 𝙾𝚕𝚎𝚑 Irfaan Official,𝙿𝚎𝚖𝚋𝚞𝚊𝚝𝚊𝚗 𝙿𝚛𝚘𝚓𝚎𝚌𝚝 𝙱𝚘𝚝 𝙸𝚗𝚒 𝙼𝚞𝚕𝚊𝚒 𝙳𝚊𝚛𝚒 𝚃𝚊𝚗𝚐𝚐𝚊𝚕 22 𝙰𝚐𝚞𝚜𝚝𝚞𝚜 2022,𝚃𝚎𝚛𝚒𝚖𝚊 𝙺𝚊𝚜𝚒𝚑 𝚈𝚊𝚗𝚐 𝚃𝚎𝚕𝚊𝚑 𝙼𝚎𝚖𝚋𝚊𝚗𝚝𝚞 𝚂𝚊𝚢𝚊 𝚂𝚎𝚙𝚎𝚗𝚞𝚑 𝙷𝚊𝚝𝚒 𝙳𝚊𝚕𝚊𝚖 𝙼𝚎𝚖𝚋𝚞𝚊𝚝 𝙿𝚛𝚘𝚓𝚎𝚌𝚝 𝚂𝚌𝚛𝚒𝚙𝚝 𝙸𝚗𝚒\n
+⫰⫯⫰ 𝐃𝐚𝐭𝐞 𝐈𝐬𝐥𝐚𝐦 : ${dateIslamic}
+⫰⫯⫰ 𝐃𝐚𝐭𝐞 : ${date}
+⫰⫯⫰ 𝐔𝐩𝐭𝐢𝐦𝐞 : ${uptime}
+⫰⫯⫰ 𝐓𝐢𝐦𝐞 : ${time}`
+const buttons1 = 'All Menu'
+const buttons2 = 'List Menu'
+const idbuttons1 = '.? all'
+const idbuttons2 = '.co'
 
-           },
-             {
-             urlButton: {
-               displayText: 'Owner',
-               PhoneNumber: 'https://wa.me/6289509960601'
-             }
-
-           },
-           {
-             quickReplyButton: {
-               displayText: 'ALL MENU',
-               id: '.allmenut',
-             }
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
-  } catch (e) {
-    conn.reply(m.chat, 'Maaf, menu sedang error', m)
-    throw e
-  }
+conn.send2ButtonLoc(m.chat, bcbg, anu, footer, buttons1, idbuttons1, buttons2, idbuttons2, m)
 }
 handler.help = ['menu']
 handler.tags = ['main']
 handler.command = /^(menu)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 3
+handler.register = true
 
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
 
 function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+function pickRandom(list) {
+     return list[Math.floor(Math.random() * list.length)]
+  }
+function ucapan() {
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  res = "Selamat DiniHari"
+  if (time >= 4) {
+    res = "Selamat Pagi"
+  }
+  if (time > 10) {
+    res = "Selamat Siang"
+  }
+  if (time >= 15) {
+    res = "Selamat Sore"
+  }
+  if (time >= 18) {
+    res = "Selamat Malam"
+  }
+  return res
 }
